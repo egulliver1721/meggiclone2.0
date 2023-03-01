4c2ad4eb4b31a470e0b2f8350192902a53efaeb7
djnovin <hello@novinnoori.com>
2023-03-01 12:24:00 +1100

    added trpc and prisma to backend

--- apps/frontend/src/components/header.tsx
+++ apps/frontend/src/components/header.tsx
@@ -1,21 +1,74 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import anime from 'animejs';
 
-export default function Header(props) {
-  return (
-    <div
-      className="heroImage"
-      style={{
-        '--img': `url(${props.heroImage}), 
-      linear-gradient(#e66465, #9198e5)`,
-      }}
-    >
-      <h1 className="heading">We're getting organised over here.</h1>
-      <p className="description">The ultimate organisation tool for you and your family.</p>
-      <div className="buttonDiv center">
-        <a className="button" href="www.google.com">
-          Shop Tags
-        </a>
-      </div>
-    </div>
-  );
+interface HeaderProps {
+  heroImage: string;
 }
+
+const Header = (props: HeaderProps) => {
+  useEffect(() => {
+    anime({
+      targets: '.heading ',
+      translateY: [10, 0],
+      opacity: [0, 1],
+      easing: 'easeOutQuad',
+      duration: 1000,
+    });
+
+    anime({
+      targets: '.description',
+      translateY: [10, 0],
+      opacity: [0, 1],
+      easing: 'easeOutQuad',
+      duration: 1000,
+      delay: 500,
+    });
+  }, []);
+
+  const heroImage = document.querySelector('.heroImage');
+  const button = document.querySelector('.button');
+
+  if (heroImage && button) {
+    button.addEventListener('mouseenter', () => {
+      anime({
+        targets: heroImage,
+        scale: 1.1,
+        easing: 'easeOutQuad',
+        duration: 500,
+      });
+    });
+
+    button.addEventListener('mouseleave', () => {
+      anime({
+        targets: heroImage,
+        scale: 1,
+        easing: 'easeOutQuad',
+        duration: 500,
+      });
+    });
+  }
+
+  return (
+    <>
+      <div className="imageContainer">
+        <div
+          className="heroImage"
+          style={{
+            '--img': `url(${props.heroImage}), 
+      linear-gradient(#e66465, #9198e5)`,
+          }}
+        >
+          <h1 className="heading">We're getting organised over here.</h1>
+          <p className="description">The ultimate organisation tool for you and your family.</p>
+          <div className="buttonDiv center">
+            <a className="button" href="www.google.com">
+              Shop Tags
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
