6b0bd72c5fe1498c0754a5ca9ada3ed949cc2c86
djnovin <hello@novinnoori.com>
2023-03-02 23:52:14 +1100

    resolved bugs and added turbo repo instead of nx

---
+++ apps/server/src/trpc.ts
@@ -0,0 +1,33 @@
+import { inferAsyncReturnType, initTRPC } from '@trpc/server';
+import * as trpcExpress from '@trpc/server/adapters/express'; 
+import { PrismaClient } from '.prisma/client'
+import {
+    newsletterRouter, 
+} from './routes';
+
+const prisma = new PrismaClient();
+
+export const createContext = ({
+    req,
+    res,
+}: trpcExpress.CreateExpressContextOptions) => ({
+    req,
+    res,
+    prisma
+});
+
+type Context = inferAsyncReturnType<typeof createContext>;
+
+const t = initTRPC.context<Context>().create();
+
+export const router = t.router;
+export const middleware = t.router;
+export const publicProcedure = t.procedure; 
+
+export const appRouter = router({
+    user: newsletterRouter,
+});
+
+// Export type router type signature,
+// NOT the router itself.
+export type AppRouter = typeof appRouter;
