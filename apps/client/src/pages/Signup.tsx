d2d1a85f768fc3ed3e19abc48137885ab64a7cc2
djnovin <hello@novinnoori.com>
2023-03-05 23:51:47 +1100

    login route

---
+++ apps/client/src/pages/Signup.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+const Signup = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleSignup = async () => {
+    try {
+      const baseURL = 'http://localhost:8000'; // Add base URL
+      await axios.post(`${baseURL}/signup`, { email, password, firstName, lastName }); // Include base URL in request
+      console.log('User created successfully');
+    } catch (error) {
+      console.error(error);
+      setErrorMessage('Signup failed. Please try again.');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Signup</h2>
+      <div>
+        <label>Email:</label>
+        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </div>
+      <div>
+        <label>First Name:</label>
+        <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+      </div>
+      <div>
+        <label>Last Name:</label>
+        <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+      </div>
+      <button onClick={handleSignup}>Signup</button>
+      {errorMessage && <p>{errorMessage}</p>}
+    </div>
+  );
+};
+
+export default Signup;
