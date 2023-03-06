721850ed3b1d59c0e0eff9bb61c99b524650232f
djnovin <hello@novinnoori.com>
2023-03-06 19:03:58 +1100

    added input validation to form

--- apps/client/src/components/user/index.tsx
+++ apps/client/src/components/user/index.tsx
@@ -3,14 +3,45 @@ import style from './user.module.scss';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { z } from 'zod';
 
 export const loggedInAtom = atom(false);
 
+interface FormValues {
+  email: string;
+  password: string;
+}
+
+interface FormErrors {
+  email?: string;
+  password?: string;
+  message?: string;
+}
+
+const schema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
 const User = () => {
   const [loggedIn, setLoggedIn] = useAtom(loggedInAtom);
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [formValues, setFormValues] = useState<FormValues>({ email: '', password: '' });
+  const [errors, setErrors] = useState<FormErrors>({});
+
+  const handleInputChange = (event: any) => {
+    const { name, value } = event.target;
+    setFormValues({ ...formValues, [name]: value });
+
+    // Validate the input and update the errors state
+    try {
+      const updatedValues = { ...formValues, [name]: value };
+      schema.parse(updatedValues); // throws an error if the input is invalid
+      setErrors({ ...errors, [name]: undefined });
+    } catch (error: any) {
+      const fieldError = error.errors[0]?.message;
+      setErrors({ ...errors, [name]: fieldError });
+    }
+  };
 
   console.log(loggedIn);
 
@@ -24,15 +55,17 @@ const User = () => {
   const handleLogin = async () => {
     try {
       const baseURL = 'http://localhost:8000';
-      const response = await axios.post(`${baseURL}/login`, { email: username, password });
+      const response = await axios.post(`${baseURL}/login`, formValues);
       const { token } = response.data;
       localStorage.setItem('token', token);
       setLoggedIn(true);
       console.log('Login successful');
-    } catch (error) {
+    } catch (error: any) {
       console.error(error);
-      setErrorMessage('Login failed. Please try again.');
       console.log('Login failed');
+      if (error.formErrors) {
+        setErrors(error.formErrors.fieldErrors);
+      }
     }
   };
 
@@ -63,17 +96,18 @@ const User = () => {
         ) : (
           <>
             <h2>Login</h2>
-            <label>Username:</label>
-            <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
-            <label>Password:</label>
-            <input
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
+            <div>
+              <label htmlFor="email">Email:</label>
+              <input type="email" name="email" value={formValues.email} onChange={handleInputChange} />
+              {errors.email && <span className="error">{errors.email}</span>}
+            </div>
+            <div>
+              <label htmlFor="password">Password:</label>
+              <input type="password" name="password" value={formValues.password} onChange={handleInputChange} />
+              {errors.password && <span className="error">{errors.password}</span>}
+            </div>
             <button onClick={handleLogin}>Login</button>
-            {errorMessage && <p>{errorMessage}</p>}
+            {errors.message && <span className="error">{errors.message}</span>}
             <p>Don't have an account?</p>
             <Link to="/Signup">Signup</Link>
           </>
