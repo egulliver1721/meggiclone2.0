c71953d5e2918337229670c928fdd05973b8df4e
djnovin <hello@novinnoori.com>
2023-03-13 17:35:59 +1100

    Dockerfile for the server added

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc';
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // Allow connections from outside the container
+    strictPort: true, // Throw an error if the port is already in use
+    port: 5173, // The port to use
+  }
 });
