759b1197c97da61e7a4a21638f6c701426b8a066
djnovin <hello@novinnoori.com>
2023-03-13 18:02:47 +1100

    added cookie accept form

--- apps/client/src/pages/Root.tsx
+++ apps/client/src/pages/Root.tsx
@@ -1,71 +1,26 @@
-import { useState, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import Banner from '../components/banner';
 import Navbar from '../components/navbar';
 import Footer from '../components/footer';
 import style from './root.module.scss';
 import React from 'react';
+import CookiesNotification from '../components/cookies';
 
 const Root = () => {
-  //   const [cartItems, setCartItems] = useState([]);
-  //   const [tagPattern, setTagPattern] = useState(null);
-
-  //   const handleSubmit = (e) => {
-  //     e.preventDefault();
-  //     const exists = cartItems.some((item) => item.pattern === tagPattern);
-  //     if (!exists) {
-  //       const selectedTag = tagData.find((tag) => tag.pattern === tagPattern);
-  //       if (selectedTag) {
-  //         setCartItems([...cartItems, selectedTag]);
-  //       }
-  //     }
-  //   };
-
-  // console.log()
-
-  //   useEffect(() => {
-  //     console.log(cartItems);
-  //   }, [cartItems]);
-
-  // function handleIncrement(pattern) {
-  //   const updatedCartItems = cartItems.map((tag) => {
-  //     if (tag.pattern === pattern) {
-  //       return { ...tag, quantity: tag.quantity + 1 };
-  //     } else {
-  //       return tag;
-  //     }
-  //   });
-  //   setCartItems(updatedCartItems);
-  // }
-
-  // const handleDecrement = (pattern) => {
-  //   const updatedCartItems = cartItems.map((tag) => {
-  //     if (tag.pattern === pattern) {
-  //       const newQuantity = Math.max(1, tag.quantity - 1);
-  //       return { ...tag, quantity: newQuantity };
-  //     } else {
-  //       return tag;
-  //     }
-  //   });
-  //   setCartItems(updatedCartItems);
-  // };
-
-  // const handleRemove = (pattern) => {
-  //   const removeCartItem = cartItems.filter((tag) => tag.pattern !== pattern);
-  //   setCartItems(removeCartItem);
-  // };
-
   return (
-    <div className={style.root}>
-      <header>
-        <Banner text="Free Shipping Across Australia" />
-        <Navbar />
-      </header>
-      <section className={style.container}>
-        <Outlet />
-      </section>
-      <Footer />
-    </div>
+    <>
+      <CookiesNotification />
+      <div className={style.root}>
+        <header>
+          <Banner text="Free Shipping Across Australia" />
+          <Navbar />
+        </header>
+        <section className={style.container}>
+          <Outlet />
+        </section>
+        <Footer />
+      </div>
+    </>
   );
 };
 
