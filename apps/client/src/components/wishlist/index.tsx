290eb22a71482919b279223e6ac63b94137a3826
djnovin <hello@novinnoori.com>
2023-03-17 07:28:40 +1100

    added more stuff

--- apps/client/src/components/wishlist/index.tsx
+++ apps/client/src/components/wishlist/index.tsx
@@ -1,25 +1,26 @@
 import style from './wishlist.module.scss';
-import {useAtom} from "jotai/index";
-import {loggedInAtom} from "../user";
-import {Link} from "react-router-dom";
+import { useAtom } from 'jotai/index';
+import { loggedInAtom } from '../user';
+import { Link } from 'react-router-dom';
+import React from 'react';
 
 const Index = () => {
-    const [loggedIn, setLoggedIn] = useAtom(loggedInAtom);
+  const [loggedIn, setLoggedIn] = useAtom(loggedInAtom);
 
-    return (
-        <div className={style.wishlist}>
-            <div className={style.container}>
-                {loggedIn ? (
-                    <h1>Wishlist</h1>
-                ) : (
-                    <>
-                        <p>Sign in to view</p>
-                        <Link to="/login">Login</Link>
-                    </>
-                )}
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className={style.wishlist}>
+      <div className={style.container}>
+        {loggedIn ? (
+          <h1>Wishlist</h1>
+        ) : (
+          <>
+            <p>Sign in to view</p>
+            <Link to="/login">Login</Link>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Index;
