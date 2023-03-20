a175f1e12cf95b8628137a4d273fa9a48c1f01e4
egulliver1721 <118499423+egulliver1721@users.noreply.github.com>
2023-03-20 15:17:08 +0800

    deleted old code, added basic footer styling

--- apps/client/src/components/footer/index.tsx
+++ apps/client/src/components/footer/index.tsx
@@ -1,17 +1,26 @@
 import { Link } from 'react-router-dom';
 import style from './footer.module.scss';
 import React from 'react';
+import { BsInstagram, BsLinkedin, BsFacebook } from 'react-icons/bs';
 
 const Footer = () => {
   return (
     <footer>
       <div className={style.grid}>
         <div className={style.griditem}>
+          <p className={style.logo}>M.</p>
+        </div>
+        <div className={style.griditem}>
+          <div className={style.socialIcons}>
+            <BsInstagram className={style.socialIcons} />
+            <BsLinkedin className={style.socialIcons} />
+            <BsFacebook className={style.socialIcons} />
+          </div>
+        </div>
+        <div className={style.griditem}>
           <Link className={style.link} to="/Terms">
             Terms and Conditions
           </Link>
-        </div>
-        <div className={style.griditem}>
           <Link className={style.link} to="/Privacy">
             Privacy Policy
           </Link>
